c3f6478f700f205154572804b647984793af1c2e
finnshort <finn.d.short@gmail.com>
2017-06-09 15:01:20 -0700

    up to imm data and pure rendering

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      // had to update from loader: 'babel'
+      loader: 'react-hot-loader!babel-loader'
+    }]
+  },
+  resolve: {
+    // had to update from extensions: ['', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx']
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
